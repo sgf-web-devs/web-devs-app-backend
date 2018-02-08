8959519893d554c988ca7280e3184cadda6514c4
Levi Zitting <levizitting5663@gmail.com>
2018-02-07 21:13:21 -0600

    Install Laravel and remove laravel frontend starting content

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+      <title>SGF Web Devs</title>
+    </head>
+    <body>
+
+    </body>
+</html>
--- src/data/prizes.js
+++
@@ -1,7 +0,0 @@
-export const prizes = [
-	'Prize 1',
-	'Prize 2',
-	'Prize 3',
-	'Prize 4',
-	'Prize 5'
-];
