2bf3421a1e822e768f28db452a9fccdbf7f01d55
Levi Zitting <levizitting5663@gmail.com>
2018-02-08 22:02:08 -0600

    Update users migration and add Prize model, controller, and migration

--- database/migrations/2018_02_09_035426_create_prizes_table.php
+++ database/migrations/2018_02_09_035426_create_prizes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreatePrizesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('prizes', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->text('bio')->nullable();
-            $table->json('events');
-            $table->string('profile-photo');
-            $table->rememberToken();
+            $table->string('name', 250);
+            $table->boolean('enabled')->default(1);
             $table->timestamps();
         });
     }
@@ -33,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('prizes');
     }
 }
