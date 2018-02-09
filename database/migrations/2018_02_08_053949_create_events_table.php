f86399ee31e896bbc068afeb0c098c02a0fba5d9
Levi Zitting <levizitting5663@gmail.com>
2018-02-08 21:54:06 -0600

    Add Event model, controller, and migration

---
+++ database/migrations/2018_02_08_053949_create_events_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id');
+						$table->string('name', 250);
+            $table->geometry('location');
+            $table->text('description');
+						$table->dateTime('date');
+						$table->json('attendees');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
