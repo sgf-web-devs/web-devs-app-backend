54e88a920123d8e8f0e42063b30fde7826310f21
Levi Zitting <levizitting5663@gmail.com>
2018-02-10 19:31:30 -0600

    Add SVG via import statement

--- resources/assets/angular/src/app/pages/dashboard/dashboard.component.ts
+++ resources/assets/angular/src/app/pages/dashboard/dashboard.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as logoImg from '../../../assets/web-devs-logo.svg';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.styl']
 })
 export class DashboardComponent implements OnInit {
-
+  public logo = logoImg;
   constructor() { }
 
   ngOnInit() {
