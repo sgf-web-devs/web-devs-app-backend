8b4047389290145f7ddab032cf7ef57d515f68b8
Levi Zitting <levizitting5663@gmail.com>
2018-02-17 16:14:45 -0600

    Add prizes page and route definition

--- resources/assets/angular/src/app/admin/admin-routing.module.ts
+++ resources/assets/angular/src/app/admin/admin-routing.module.ts
@@ -1,21 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminComponent } from './components/admin/admin.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RaffleComponent } from './pages/raffle/raffle.component';
-import { AdminComponent } from './components/admin/admin.component';
+import { PrizesComponent } from './pages/prizes/prizes.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: '', component: AdminComponent, children: [
-      { path: 'raffle', component: RaffleComponent }
+      { path: 'raffle', component: RaffleComponent },
+      { path: 'prizes', component: PrizesComponent }
   ]},
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
-  declarations: [],
 })
 export class AdminRoutingModule { }
 
-export const routingComponents = [ LoginComponent, RaffleComponent, AdminComponent ];
+export const routingComponents = [ LoginComponent, RaffleComponent, AdminComponent, PrizesComponent ];
---
+++ resources/assets/angular/src/app/admin/pages/prizes/prizes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prizes',
+  templateUrl: './prizes.component.html',
+  styleUrls: ['./prizes.component.sass']
+})
+export class PrizesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
