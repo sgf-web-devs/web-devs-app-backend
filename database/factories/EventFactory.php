20b7922cd370d5abd5666be260c4b6c83195fe01
Levi Zitting <levizitting5663@gmail.com>
2018-02-22 21:53:35 -0600

    Add db seeding for events, users, and prizes

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -4,10 +4,10 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Event::class, function(Faker $faker) {
 	return [
-		'name' => $faker->unique()->words(4),
+		'name' => $faker->unique()->words(4, true),
 		'lat' => 37.2119562,
 		'lng' => -93.2925957,
 		'description' => $faker->randomHtml(1),
-		'date' => $faker->dateTimeBetween($startDate = '-1 years', $endDate = '+ 1 years')
+		'date' => $faker->dateTimeBetween('-1 years','+ 1 years')
 	];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,13 +4,16 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
-    }
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		// $this->call(UsersTableSeeder::class);
+		factory(App\User::class, 50)->create();
+		factory(App\Event::class, 10)->create();
+		factory(App\Prize::class, 20)->create();
+	}
 }
