aa637aff12ebeedc85e44e54c86f6d6fb5294958
Mike Copanos <mcopanos@gmail.com>
2018-04-16 22:17:08 -0500

    pusher to pusher

---
+++ app/Events/AttendeeCheckIn.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Events;
+use App\Attendee;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class AttendeeCheckIn implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $attendee;
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(Attendee $attendee)
+    {
+        $this->attendee = $attendee;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('attendee-check-in.'.$attendee->id);
+    }
+
+    public function broadcastWith()
+    {
+        return [
+          'user' => [
+              'name' => $this->attendee->user->name,
+              'avatar' => $this->user->avatar
+          ]
+        ];
+    }
+}
--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Events\AttendeeCheckIn;
 
 class EventController extends Controller
 {
     public function checkin(Request $request) {
+
+        event(new AttedeeCheckIn($attendee));
+        
         return response()->json(true);
     }
 }
