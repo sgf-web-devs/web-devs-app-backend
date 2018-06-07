08cdad8aacdb82c8046effb2a442ececa323f3b6
Levi Zitting <levizitting5663@gmail.com>
2018-06-06 23:51:04 -0500

    Fix build issue on linux

--- resources/assets/angular/src/app/providers/attendee/attendee.service.ts
+++ resources/assets/angular/src/app/providers/attendee/attendee.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/observable';
+import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 import * as Pusher from 'pusher-js';
 
